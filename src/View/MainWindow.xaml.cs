5d0c8284bc18104f348380c1e2159b6c6e38b611
Динар Каримов < >
2017-03-23 16:51:13 +0300

    Adding "Operator Guide" and Main window into project

--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MidSurfGenerator
+namespace View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,83 @@ namespace MidSurfGenerator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void View_Import(object sender, RoutedEventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog importDlg = new OpenFileDialog();
+
+            importDlg.InitialDirectory = "C:\\";
+            importDlg.Filter = "All files (*.*)|*.*|MSG Models (*.msgm)|*.msgm";
+            importDlg.FilterIndex = 2;
+            importDlg.RestoreDirectory = true;
+
+            if (importDlg.ShowDialog() == null)
+            {
+                try
+                {
+                    if ((myStream = importDlg.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            //TODO : read file
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //TODO : replace with dict
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void View_Export(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog exportDlg = new SaveFileDialog();
+
+            exportDlg.InitialDirectory = "C:\\";
+            exportDlg.Filter = "All files (*.*)|*.*|MSG Models (*.msgm)|*.msgm";
+            exportDlg.FilterIndex = 2;
+            exportDlg.RestoreDirectory = true;
+
+            if (exportDlg.ShowDialog() == null)
+            {
+                try
+                {
+                   //TODO : save file
+                }
+                catch (Exception ex)
+                {
+                    //TODO : replace with dict
+                    MessageBox.Show("Error: Could not save file on disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void view_Generate(object sender, RoutedEventArgs e)
+        {
+            Point endPoint = new Point(this.Width, this.Height-180);
+
+            PathFigure pathFigure = new PathFigure
+            {
+                StartPoint = new Point(0, 0),
+                IsClosed = false
+            };
+
+            Vector vector = endPoint - pathFigure.StartPoint;
+            Point point1 = new Point(pathFigure.StartPoint.X + vector.X / 2, pathFigure.StartPoint.Y);
+            Point point2 = new Point(pathFigure.StartPoint.X + vector.X / 1.5, pathFigure.StartPoint.Y + vector.Y / 0.95);
+            BezierSegment curve = new BezierSegment(point1, point2, endPoint, true);
+
+            PathGeometry path = new PathGeometry();
+            path.Figures.Add(pathFigure);
+            pathFigure.Segments.Add(curve);
+            pathMain.Data = path;
+
+
         }
     }
 }
