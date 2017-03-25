cb38c4aa6ecbdff83320b934957bcddc7665ea6a
Linserous <ilin_nnov_pf@mail.ru>
2017-03-25 21:51:36 +0300

    Re-created project

--- src/Solver/Properties/AssemblyInfo.cs
+++ src/Solver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solver")]
+[assembly: AssemblyTitle("Algorithm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solver")]
+[assembly: AssemblyProduct("Algorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6bb9b7f9-85ac-47f0-a1b1-282aedcf100d")]
+[assembly: Guid("eb8eefb4-f72a-47c4-a31b-43a9392d62c1")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ src/View/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public class Class1
+    {
+    }
+}
