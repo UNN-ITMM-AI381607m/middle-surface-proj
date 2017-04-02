3411053fc99df5fb09f4022e8dc55ca06290236f
Linserous <ilin_nnov_pf@mail.ru>
2017-04-02 23:41:02 +0300

    Added solverdata

---
+++ src/Solver/ISolverData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidSurface.Component;
+using MidSurface.Primitive;
+
+namespace Solver
+{
+    public interface ISolverData
+    {
+        List<ISegment> GetSegments();
+    }
+}
---
+++ src/Solver/SolverData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidSurface.Primitive;
+using MidSurface.Component;
+
+namespace Solver
+{
+    public class SolverData: ISolverData
+    {
+        List<ISegment> segments;
+
+        public SolverData(IModel model)
+        {
+            segments = model.GetCanvasData().ToList<ISegment>();
+        }
+
+        public List<ISegment> GetSegments()
+        {
+            return segments;
+        }
+    }
+}
