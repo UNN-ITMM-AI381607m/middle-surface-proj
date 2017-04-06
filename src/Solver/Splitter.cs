31e9a72ed49902dc9b61f5d1ab038d738dca8bbc
sevoster <dmitry.polunin@intel.com>
2017-04-06 20:40:37 +0300

    Implemented splitter

--- src/Solver/Splitter.cs
+++ src/Solver/Splitter.cs
@@ -9,12 +9,29 @@ namespace Solver
 {
     public class Splitter: ISplitter
     {
-        public Splitter() { }
+        double m_lastPointInaccuracy;
+        public Splitter(double lastPointInaccuracy = 0.1) {
+            m_lastPointInaccuracy = lastPointInaccuracy;
+        }
 
         public List<ICustomPoint> Split(IEnumerable<ISegment> segments, double accuracy)
         {
-            List<ICustomPoint> custompoints = new List<ICustomPoint>();
-            return custompoints;
+            List<ICustomPoint> customPoints = new List<ICustomPoint>();
+            for (int i = 0; i < segments.Count(); i++)
+            {
+                double t = 0;
+                while (t <= 1)
+                {
+                    customPoints.Add(new CustomPoint(i, t));
+                    t += accuracy;
+                }
+                if (1 - t + accuracy <= m_lastPointInaccuracy * accuracy)
+                {
+                    customPoints.RemoveAt(customPoints.Count() - 1);
+                }
+                customPoints.Add(new CustomPoint(i, 1));
+            }
+            return customPoints;
         }
     }
 }
