9c07bef749ddc657ce5da4c35b6561449f244609
Linserous <ilin_nnov_pf@mail.ru>
2017-04-08 18:07:30 +0300

    Tests

--- src/UnitTests/Properties/AssemblyInfo.cs
+++ src/UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyTitle("UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyProduct("UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb3942d2-530e-4f8b-89d2-dfadb42d2c1e")]
+[assembly: Guid("544ec0bf-cb0c-4c1a-bc78-e829d95b8ba8")]
 
 // Version information for an assembly consists of the following four values:
 //
