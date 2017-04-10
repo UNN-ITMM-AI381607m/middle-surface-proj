629ca6eaa22ab3f8c272f6da9349620fe99253e5
Linserous <ilin_nnov_pf@mail.ru>
2017-04-10 20:35:44 +0300

    fix

--- src/Solver/IMSPointFinder.cs
+++ src/Solver/IMSPointFinder.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MidSurfaceNameSpace.Solver
 {
     public interface IMSPointFinder
     {
         List<IMSPoint> FindMSPoints(List<ICustomLine> customline);
-        IMSPoint FindBisectorPoint(ICustomLine customline1, ICustomLine customline2);
+        IMSPoint GetMSPoint(Vector vector, Point point, ICustomLine line);
     }
 }
