66f7fd6e889611edc106413aa09f873090deda35
Elizaveta Pronina <elizaveta.pronina@intel.com>
2017-05-18 23:40:29 +0300

    updated accuracy for Detalizer

--- src/Solver/Algorithm.cs
+++ src/Solver/Algorithm.cs
@@ -58,7 +58,8 @@ namespace MidSurfaceNameSpace.Solver
             maxLengthModel = FindMaxLength(solverdata.GetContours(), 0.01);
 
             BaseAlgorithm baseAlgorithm = new BaseAlgorithm();
-            List<IMSPoint> msPoints = baseAlgorithm.Run(solverdata, splitterAccuracy * maxLengthModel, detalizerAccuracy);
+            List<IMSPoint> msPoints = baseAlgorithm.Run(solverdata, splitterAccuracy * maxLengthModel, 
+                detalizerAccuracy * maxLengthModel);
 
             //Graph msGraph = ConstructGraph(msPoints, baseAlgorithm.GetSimplifiedModel());
             //msGraph.RemoveCycles(maxCycleSize);
